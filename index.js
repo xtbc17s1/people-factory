62a65161a2bd7fbbb615a743fdcc5cb63dad7846
Dave Strus <dave@getfretless.com>
2017-05-15 11:26:21 -0400

    Append the name as a proper DOM element

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ personForm.onsubmit = (ev) => {
 
   const personName = form.personName.value
 
-  details.innerHTML = `<em>${personName}</em>`
+  // details.innerHTML = `<em>${personName}</em>`
+
+  const em = document.createElement('em')
+  em.textContent = personName
+  details.appendChild(em)
 }

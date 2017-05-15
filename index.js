58dfc51ddb0293cb96b0b8455100e09dac3a36a3
Dave Strus <dave@getfretless.com>
2017-05-15 15:52:05 -0400

    Use document.createElement and appendChild

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ personForm.onsubmit = (ev) => {
   const details = document.querySelector('.details')
 
   const personName = form.personName.value
-  details.innerHTML = `<em>${personName}</em>`
+
+  const em = document.createElement('em')
+  em.textContent = personName
+
+  details.appendChild(em)
 }

4f477818685ed5b44423270ae9e50d5aa16bd72a
Dave Strus <dave@getfretless.com>
2017-05-15 11:46:46 -0400

    Show all form values in details

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ personForm.onsubmit = (ev) => {
   const details = document.querySelector('.details')
 
   const personName = form.personName.value
+  const hairColor = form.hairColor.value
+  const age = form.age.value
+  const birthplace = form.birthplace.value
+
+  const colorDiv = `
+    <div style="height: 50px; width: 100px; background-color: ${hairColor}"></div>
+  `
 
   // details.innerHTML = `<em>${personName}</em>`
 
-  const em = document.createElement('em')
-  em.textContent = personName
-  details.appendChild(em)
+  // const em = document.createElement('em')
+  // em.textContent = personName
+  // details.appendChild(em)
+
+  details.innerHTML = `
+    <ul>
+      <li>Name: ${personName}</li>
+      <li>Hair Color: ${colorDiv}</li>
+      <li>Age: ${age}</li>
+      <li>Birthplace: ${birthplace}</li>
+    </ul>
+  `
 }

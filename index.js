f6de160848e4778008b40f8cea23ed590e0fdaca
Dave Strus <dave@getfretless.com>
2017-05-15 15:59:56 -0400

    Display hair color

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ personForm.onsubmit = (ev) => {
   const details = document.querySelector('.details')
 
   const personName = form.personName.value
+  const hairColor = form.hairColor.value
 
   const em = document.createElement('em')
   em.textContent = personName
 
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = hairColor
+  colorDiv.style.height = '50px'
+  colorDiv.style.width = '100px'
+
   details.appendChild(em)
+  details.appendChild(colorDiv)
 }

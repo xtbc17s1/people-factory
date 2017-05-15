e1f9f5b766e9be2d70f7a4ffd07e66bdb646f000
Dave Strus <dave@getfretless.com>
2017-05-15 11:19:56 -0400

    Add person name to details

---
+++ index.js
@@ -0,0 +1,11 @@
+const personForm = document.querySelector('form')
+
+personForm.onsubmit = (ev) => {
+  ev.preventDefault()
+  const form = ev.target
+  const details = document.querySelector('.details')
+
+  const personName = form.personName.value
+
+  details.innerHTML = '<em>' + personName + '</em>'
+}

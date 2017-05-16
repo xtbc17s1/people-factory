21e4fcb723b56692685d1b61e4f51594f4007cce
Dave Strus <dave@getfretless.com>
2017-05-16 10:24:01 -0400

    Wrap everything in an IIFE

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-const personForm = document.querySelector('form')
+(function() {
+  const personForm = document.querySelector('form')
 
-const renderColor = (hairColor) => {
-  const colorDiv = document.createElement('div')
-  colorDiv.style.height = '50px'
-  colorDiv.style.width = '100px'
-  colorDiv.style.backgroundColor = hairColor
-  return colorDiv
-}
-
-const renderListItem = (name, value) => {
-  const li = document.createElement('li')
-  li.innerHTML = `${name}: ${value}`
-  return li
-}
-
-const renderList = (person) => {
-  const list = document.createElement('ul')
-  Object.keys(person).map((fieldName, _i, _keys) => {
-    let li = renderListItem(fieldName, person[fieldName])
-    list.appendChild(li)
-  })
-
-  return list
-}
-
-const handleSubmit = (ev) => {
-  ev.preventDefault()
-  const form = ev.target
-  const details = document.querySelector('.details')
-
-  const person = {
-    name: form.personName.value,
-    hairColor: renderColor(form.hairColor.value).outerHTML,
-    age: form.age.value,
-    birthplace: form.birthplace.value,
+  const renderColor = (hairColor) => {
+    const colorDiv = document.createElement('div')
+    colorDiv.style.height = '50px'
+    colorDiv.style.width = '100px'
+    colorDiv.style.backgroundColor = hairColor
+    return colorDiv
   }
 
-  const list = renderList(person)
+  const renderListItem = (name, value) => {
+    const li = document.createElement('li')
+    li.innerHTML = `${name}: ${value}`
+    return li
+  }
 
-  details.appendChild(list)
-}
+  const renderList = (person) => {
+    const list = document.createElement('ul')
+    Object.keys(person).map((fieldName, _i, _keys) => {
+      let li = renderListItem(fieldName, person[fieldName])
+      list.appendChild(li)
+    })
 
-personForm.addEventListener('submit', handleSubmit)
+    return list
+  }
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    const form = ev.target
+    const details = document.querySelector('.details')
+
+    const person = {
+      name: form.personName.value,
+      hairColor: renderColor(form.hairColor.value).outerHTML,
+      age: form.age.value,
+      birthplace: form.birthplace.value,
+    }
+
+    const list = renderList(person)
+
+    details.appendChild(list)
+  }
+
+  personForm.addEventListener('submit', handleSubmit)
+})()
